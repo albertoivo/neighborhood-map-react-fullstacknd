1f2ccb11736599e6f8e52fbd7f3061947148cef4
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-10-16 07:16:10 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,95 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { locations } from './locations.js'
+
+/* global google */
 
 class App extends Component {
+  componentDidMount() {
+    // Connect the initMap() function within this class to the global window context,
+    // so Google Maps can invoke it
+    window.initMap = this.initMap
+    // Asynchronously load the Google Maps script, passing in the callback reference
+    loadJS(
+      'https://maps.googleapis.com/maps/api/js?key=AIzaSyCfoloq_rkZTlV9bMcNOCptegicVqCqZ4A&callback=initMap',
+      this.mapFail
+    )
+  }
+
+  initMap() {
+    let self = this
+    let markers = []
+
+    self.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: -15.8012908, lng: -47.8675807 },
+      zoom: 13
+    })
+
+    const defaultIcon = makeMarkerIcon('0091ff')
+    const highlightedIcon = makeMarkerIcon('FFFF24')
+
+    let bounds = new google.maps.LatLngBounds()
+
+    for (let i = 0; i < locations.length; i++) {
+      let position = locations[i].location
+      let title = locations[i].title
+      let foursquare_id = locations[i].foursquare
+      var foursquare_html = ''
+
+      let marker = new google.maps.Marker({
+        map: self.map,
+        position,
+        title,
+        animation: google.maps.Animation.DROP,
+        id: i,
+        icon: defaultIcon
+      })
+
+      marker.addListener('mouseover', function() {
+        this.setIcon(highlightedIcon)
+      })
+
+      marker.addListener('mouseout', function() {
+        this.setIcon(defaultIcon)
+      })
+
+      markers.push(marker)
+
+      bounds.extend(markers[i].position)
+    }
+
+    self.map.fitBounds(bounds)
+  }
+
+  mapFail() {
+    alert('Sorry. Google Maps has failed. Please refresh this page.')
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div id="map" />
   }
 }
 
-export default App;
+export default App
+
+function loadJS(src, mapFail) {
+  var ref = window.document.getElementsByTagName('script')[0]
+  var script = window.document.createElement('script')
+  script.src = src
+  script.onerror = mapFail
+  script.async = true
+  ref.parentNode.insertBefore(script, ref)
+}
+
+const makeMarkerIcon = markerColor => {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' +
+      markerColor +
+      '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21, 34)
+  )
+  return markerImage
+}
---
+++ src/locations.js
@@ -0,0 +1,37 @@
+export const locations = [
+  {
+    title: 'National Congress',
+    location: { lat: -15.7997118, lng: -47.8641627 },
+    foursquare: '4c0b9644340720a1a94c8893'
+  },
+  {
+    title: 'Mané Garrincha Stadium',
+    location: { lat: -15.7835194, lng: -47.8992105 },
+    foursquare: '4e68bac3152001e1f73bbc72'
+  },
+  {
+    title: 'Toinha Brasil Show',
+    location: { lat: -15.8228551, lng: -47.9568887 },
+    foursquare: '5ace9a8fa879213b87f20d36'
+  },
+  {
+    title: 'Cathedral of Brasília',
+    location: { lat: -15.7983419, lng: -47.8755394 },
+    foursquare: '4bd9e2a32a3a0f471fb7a8b6'
+  },
+  {
+    title: 'Café Cristina',
+    location: { lat: -15.7833516, lng: -47.8785346 },
+    foursquare: '4c0684e0cf8c76b0b6963a65'
+  },
+  {
+    title: 'City Park',
+    location: { lat: -15.8003432, lng: -47.9078002 },
+    foursquare: '4b816f4bf964a520cfa530e3'
+  },
+  {
+    title: 'National Theater',
+    location: { lat: -15.7922213, lng: -47.8802482 },
+    foursquare: '4bc24e154cdfc9b65cf39521'
+  }
+]
