0f8d5bb489f5d640fb4050991574f76ca73af7f8
Alberto <alberto.vieira@serpro.gov.br>
2018-10-25 12:23:28 -0300

    foursquare OK

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import './App.css'
-import Menu from './Menu'
-import { locations } from '../locations.js'
+import React, { Component } from "react"
+import "./App.css"
+import Menu from "./Menu"
+import { locations } from "../locations.js"
 
 /* global google */
 
@@ -13,7 +13,8 @@ class App extends Component {
       markers: [],
       infowindow: {},
       bounds: {},
-      map: {}
+      map: {},
+      locations
     }
 
     this.chooseALocation = this.chooseALocation.bind(this)
@@ -26,7 +27,7 @@ class App extends Component {
     window.initMap = this.initMap.bind(this)
 
     loadJS(
-      'https://maps.googleapis.com/maps/api/js?key=AIzaSyCfoloq_rkZTlV9bMcNOCptegicVqCqZ4A&callback=initMap',
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCfoloq_rkZTlV9bMcNOCptegicVqCqZ4A&callback=initMap",
       this.mapFail
     )
   }
@@ -37,44 +38,55 @@ class App extends Component {
     let infowindow = new window.google.maps.InfoWindow()
     let bounds = new google.maps.LatLngBounds()
 
-    const map = new google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: -15.8012908, lng: -47.8675807 },
       zoom: 13
     })
 
-    const defaultIcon = this.makeMarkerIcon('0091ff')
-    const highlightedIcon = this.makeMarkerIcon('FFFF24')
+    const defaultIcon = this.makeMarkerIcon("0091ff")
+    const highlightedIcon = this.makeMarkerIcon("FFFF24")
 
-    for (let i = 0; i < locations.length; i++) {
-      let position = locations[i].location
-      let title = locations[i].title
+    this.state.locations.forEach((local, idx) => {
+      const position = local.location
+      const title = local.title
+
 
       let marker = new google.maps.Marker({
         map,
         position,
         title,
         animation: google.maps.Animation.DROP,
-        id: i,
+        id: idx,
         icon: defaultIcon
       })
 
-      marker.addListener('mouseover', function() {
+      marker.addListener("mouseover", function() {
         this.setIcon(highlightedIcon)
       })
 
-      marker.addListener('mouseout', function() {
+      marker.addListener("mouseout", function() {
         this.setIcon(defaultIcon)
       })
 
-      marker.addListener('click', function() {
-        self.populateInfoWindow(this, self.largeInfowindow)
+      marker.addListener("click", function() {
+        self.foursquareInfoWindow(local.foursquare)
+          .then((json) => {
+            const fsq = json.response.venue
+            if (fsq) {
+              let fsq_html = "Foursquare Likes: " + fsq.likes.summary
+              self.populateInfoWindow(this, fsq_html)
+            } else {
+              self.populateInfoWindow(this, json.meta.errorDetail)
+            }
+          })
+          .catch(err => alert(err))
         self.toggleBounce(this)
       })
 
       markers.push(marker)
 
-      bounds.extend(markers[i].position)
-    }
+      bounds.extend(markers[idx].position)
+    })
 
     map.fitBounds(bounds)
 
@@ -82,14 +94,14 @@ class App extends Component {
   }
 
   mapFail() {
-    alert('Sorry. Google Maps has failed. Please refresh this page.')
+    alert("Sorry. Google Maps has failed. Please refresh this page.")
   }
 
   render() {
     return (
       <div>
         <Menu
-          locations={locations}
+          locations={this.state.locations}
           choose={this.chooseALocation}
           hide={this.hideMarkers}
           show={this.showMarkers}
@@ -101,9 +113,9 @@ class App extends Component {
 
   makeMarkerIcon = markerColor => {
     var markerImage = new google.maps.MarkerImage(
-      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' +
+      "http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|" +
         markerColor +
-        '|40|_|%E2%80%A2',
+        "|40|_|%E2%80%A2",
       new google.maps.Size(21, 34),
       new google.maps.Point(0, 0),
       new google.maps.Point(10, 34),
@@ -112,21 +124,24 @@ class App extends Component {
     return markerImage
   }
 
-  populateInfoWindow = marker => {
+  populateInfoWindow = (marker, foursquare_html) => {
     const infowindow = this.state.infowindow
 
-    infowindow.setContent('')
     infowindow.marker = marker
 
-    infowindow.addListener('closeclick', () => {
+    infowindow.addListener("closeclick", () => {
       infowindow.marker = null
       this.stopToggleBounce(marker)
     })
-    let streetViewService = new google.maps.StreetViewService()
+
+    const streetViewService = new google.maps.StreetViewService()
     const radius = 500
 
     let infoWindowContent =
-      '<div>' + marker.title + '</div><div id="pano"></div>'
+      "<div>" +
+      marker.title +
+      '</div><div id="pano"></div><br />' +
+      foursquare_html
 
     const getStreetView = (data, status) => {
       if (status === google.maps.StreetViewStatus.OK) {
@@ -145,12 +160,12 @@ class App extends Component {
           }
         }
         new google.maps.StreetViewPanorama(
-          document.getElementById('pano'),
+          document.getElementById("pano"),
           panoramaOptions
         )
       } else {
         infowindow.setContent(
-          '<div>' + marker.title + '</div><div>No Street View Found</div>'
+          "<div>" + marker.title + "</div><div>No Street View Found</div>" + foursquare_html
         )
       }
     }
@@ -164,6 +179,34 @@ class App extends Component {
     infowindow.setContent(infoWindowContent)
 
     infowindow.open(this.map, marker)
+  }
+
+  getDate = () => {
+    let today = new Date()
+    let dd = today.getDate()
+    let mm = today.getMonth() + 1
+    let yyyy = today.getFullYear()
+
+    if (dd < 10) {
+      dd = "0" + dd
+    }
+    if (mm < 10) {
+      mm = "0" + mm
+    }
+
+    return yyyy + "" + mm + "" + dd
+  }
+
+  foursquareInfoWindow = foursquare_id => {
+    const CLIENT_ID = "1YBQ5MRZ2OAFCBWN4D5VA0BO4JFIK5HHWOO0U1O5XKR2RBNB"
+    const CLIENT_SECRET = "EMETHNPTZRNMB4HRJF4YNK3SSRE431RXOQKZT3HDLJ1AODOZ"
+
+    return fetch(
+      `https://api.foursquare.com/v2/venues/${foursquare_id}?&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${this.getDate()}`
+    )
+      .then(response => response.json())
+      .catch((err) => "erro de fsq: " + err)
+
   }
 
   toggleBounce = selectedMarker => {
@@ -209,8 +252,8 @@ class App extends Component {
 export default App
 
 const loadJS = (src, mapFail) => {
-  var ref = window.document.getElementsByTagName('script')[0]
-  var script = window.document.createElement('script')
+  var ref = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
   script.src = src
   script.onerror = mapFail
   script.async = true
