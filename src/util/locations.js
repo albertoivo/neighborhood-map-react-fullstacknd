c386f815e4d2a14faba370b252578968bc9b1ca5
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-10-28 16:58:24 -0300

    todos os paises do nossas rotas

--- src/util/locations.js
+++ src/util/locations.js
@@ -1,37 +1,152 @@
 export const locations = [
   {
     title: 'Brasil',
-    location: { lat: -15.7997118, lng: -47.8641627 },
-    foursquare: '4c0b9644340720a1a94c8893'
+    location: { lat: -15.7217175, lng: -48.0774436 },
+    foursquare: '4c0b9644340720a1a94c8893',
+    periodo: ''
   },
   {
     title: 'Argentina',
-    location: { lat: -15.7835194, lng: -47.8992105 },
-    foursquare: '4e68bac3152001e1f73bbc72'
+    location: { lat: -33.80234, lng: -64.368298 },
+    foursquare: '4e68bac3152001e1f73bbc72',
+    periodo: ''
   },
   {
     title: 'Chile',
-    location: { lat: -15.8228551, lng: -47.9568887 },
-    foursquare: '5ace9a8fa879213b87f20d36'
+    location: { lat: -25.812106, lng: -70.281501 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
   },
   {
     title: 'Peru',
-    location: { lat: -15.7983419, lng: -47.8755394 },
-    foursquare: '4bd9e2a32a3a0f471fb7a8b6'
+    location: { lat: -12.017632, lng: -74.719942 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
   },
   {
-    title: 'República Dominicana',
-    location: { lat: -15.7833516, lng: -47.8785346 },
-    foursquare: '4c0684e0cf8c76b0b6963a65'
+    title: 'Repúplica Dominicana',
+    location: { lat: 18.724027, lng: -68.831435 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
   },
   {
     title: 'Estados Unidos',
-    location: { lat: -15.8003432, lng: -47.9078002 },
-    foursquare: '4b816f4bf964a520cfa530e3'
+    location: { lat: 41.041565, lng: -101.370652 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
   },
   {
     title: 'Canadá',
-    location: { lat: -15.7922213, lng: -47.8802482 },
-    foursquare: '4bc24e154cdfc9b65cf39521'
+    location: { lat: 56.815478, lng: -100.837726 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Irlanda',
+    location: { lat: 52.573703, lng: -7.949815 },
+    foursquare: '4c0b9644340720a1a94c8893',
+    periodo: ''
+  },
+  {
+    title: 'Irlanda do Norte',
+    location: { lat: 54.727779, lng: -6.742367 },
+    foursquare: '4e68bac3152001e1f73bbc72',
+    periodo: ''
+  },
+  {
+    title: 'Escócia',
+    location: { lat: 57.407547, lng: -3.6861 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Inglaterra',
+    location: { lat: 51.528649, lng: -1.37691 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Espanha',
+    location: { lat: 40.265452, lng: -2.959221 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'França',
+    location: { lat: 46.931966, lng: 2.745004 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Holanda',
+    location: { lat: 52.255719, lng: 5.761631 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Bálgica',
+    location: { lat: 50.622611, lng: 4.804086 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Alemanha',
+    location: { lat: 50.045187, lng: 9.546536 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Noruega',
+    location: { lat: 60.556713, lng: 7.556005 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Itália',
+    location: { lat: 42.150366, lng: 13.883623 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Dinamarca',
+    location: { lat: 55.839794, lng: 11.598084 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Suécia',
+    location: { lat: 63.310559, lng: 15.694468 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Nova Zelândia',
+    location: { lat: -41.372337, lng: 173.492042 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Austrália',
+    location: { lat: -23.675192, lng: 135.91567 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'África do Sul',
+    location: { lat: -31.815137, lng: 24.298385 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Paraguai',
+    location: { lat: -24.259308, lng: -57.14566 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
+  },
+  {
+    title: 'Vaticano',
+    location: { lat: 41.902077, lng: 12.455841 },
+    foursquare: '5ace9a8fa879213b87f20d36',
+    periodo: ''
   }
 ]
