da42bf2dff4179a051f4ec4ab7ded106f0b59956
Alberto Ivo Vieira <albertoivo@gmail.com>
2018-10-29 07:18:08 -0300

    continentes

--- src/util/locations.js
+++ src/util/locations.js
@@ -3,150 +3,175 @@ export const locations = [
     title: 'Brasil',
     location: { lat: -15.7217175, lng: -48.0774436 },
     foursquare: '4c0b9644340720a1a94c8893',
-    periodo: ''
+    periodo: '',
+    continente: 'América do Sul'
   },
   {
     title: 'Argentina',
     location: { lat: -33.80234, lng: -64.368298 },
     foursquare: '4e68bac3152001e1f73bbc72',
-    periodo: ''
+    periodo: '',
+    continente: 'América do Sul'
   },
   {
     title: 'Chile',
     location: { lat: -25.812106, lng: -70.281501 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'América do Sul'
   },
   {
     title: 'Peru',
     location: { lat: -12.017632, lng: -74.719942 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'América do Sul'
   },
   {
     title: 'Repúplica Dominicana',
     location: { lat: 18.724027, lng: -68.831435 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'América Central'
   },
   {
     title: 'Estados Unidos',
     location: { lat: 41.041565, lng: -101.370652 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'América do Norte'
   },
   {
     title: 'Canadá',
     location: { lat: 56.815478, lng: -100.837726 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'América do Norte'
   },
   {
     title: 'Irlanda',
     location: { lat: 52.573703, lng: -7.949815 },
     foursquare: '4c0b9644340720a1a94c8893',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Irlanda do Norte',
     location: { lat: 54.727779, lng: -6.742367 },
     foursquare: '4e68bac3152001e1f73bbc72',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Escócia',
     location: { lat: 57.407547, lng: -3.6861 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Inglaterra',
     location: { lat: 51.528649, lng: -1.37691 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Espanha',
     location: { lat: 40.265452, lng: -2.959221 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'França',
     location: { lat: 46.931966, lng: 2.745004 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Holanda',
     location: { lat: 52.255719, lng: 5.761631 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Bálgica',
     location: { lat: 50.622611, lng: 4.804086 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Alemanha',
     location: { lat: 50.045187, lng: 9.546536 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Noruega',
     location: { lat: 60.556713, lng: 7.556005 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Itália',
     location: { lat: 42.150366, lng: 13.883623 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Dinamarca',
     location: { lat: 55.839794, lng: 11.598084 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Suécia',
     location: { lat: 63.310559, lng: 15.694468 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   },
   {
     title: 'Nova Zelândia',
     location: { lat: -41.372337, lng: 173.492042 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Oceania'
   },
   {
     title: 'Austrália',
     location: { lat: -23.675192, lng: 135.91567 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Oceania'
   },
   {
     title: 'África do Sul',
     location: { lat: -31.815137, lng: 24.298385 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'África'
   },
   {
     title: 'Paraguai',
     location: { lat: -24.259308, lng: -57.14566 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'América do Sul'
   },
   {
     title: 'Vaticano',
     location: { lat: 41.902077, lng: 12.455841 },
     foursquare: '5ace9a8fa879213b87f20d36',
-    periodo: ''
+    periodo: '',
+    continente: 'Europa'
   }
 ]
